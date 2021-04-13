5a075174b73da1fc5e1a8a36839047dce39e105a
Paul Hebert <paul@cloudfour.com>
2021-04-12 18:26:11 -0700

    Add spiky blobs

---
+++ src/art/spiky-blobs/spiky-blobs.js
@@ -0,0 +1,85 @@
+import { SVG } from "https://cdn.skypack.dev/@svgdotjs/svg.js";
+import { SqPreview } from '../../assets/js/sq-preview.js'
+import {
+  random
+} from "https://cdn.skypack.dev/@georgedoescode/generative-utils@1.0.0"; 100;
+
+export class SqSpikyBlobs extends SqPreview {
+  constructor() {
+    super();
+
+    this.canvas = SVG().addTo(this).viewbox(0, 0, 200, 100);
+  }
+
+  draw = () => {
+    addSucculent(this.canvas, 100, 50, random(10, 48));
+  }
+
+  clear = () => {
+    this.canvas.clear();
+  }
+}
+
+customElements.define("sq-spiky-blobs", SqSpikyBlobs);
+
+function addSucculent(canvas, x, y, radius) {
+  const group = canvas.group();
+  let currentSize = radius;
+  let currentRotation = random(0, 360);
+  const color = {
+    h: random(0, 360),
+    s: 80,
+    l: 40
+  };
+  let sizeDecrease = 0.1;
+
+  while (currentSize > 0) {
+    addLeaf(group, x, y, currentSize, currentRotation, color);
+    currentSize -= sizeDecrease;
+    // sizeDecrease += 0.05;
+    currentRotation += 25;
+
+    color.l += 0.2;
+
+    color.s += 0.2;
+
+    if (currentRotation > 360) currentRotation -= 360;
+  }
+}
+
+function addLeaf(group, x, y, size, rotate, color) {
+  const startPoint = `${x} ${y}`;
+  const endPoint = `${x} ${y - size}`;
+  const controlPointY = y - (size * 1) / 3;
+  const controlPointXDifferent = (size * 1) / 4;
+  const controlPoint1 = `${x - controlPointXDifferent} ${controlPointY}`;
+  const controlPoint2 = `${x + controlPointXDifferent} ${controlPointY}`;
+
+  group
+    .path(
+      `
+      M${startPoint} 
+      Q${controlPoint1} ${endPoint}
+      Q${controlPoint2} ${startPoint}
+      Z
+    `
+    )
+    .fill(hsl(color))
+    .stroke({
+      width: 0.5,
+      color: strokeColor(color)
+    })
+    .rotate(rotate, x, y);
+}
+
+function hsl(c) {
+  return `hsl(${c.h}, ${c.s}%, ${c.l}%)`;
+}
+
+function strokeColor(c) {
+  const color = { ...c };
+  color.l -= 20;
+  color.s -= 20;
+
+  return hsl(color);
+}
---
+++ src/art/spiky-blobs/spiky-blobs.md
@@ -0,0 +1,9 @@
+---
+title: Spiky Blobs
+slug: "spiky-blobs"
+tag: "sq-spiky-blobs"
+scripts:
+  - "spiky-blobs"
+---
+
+A happy accident when working on succulents.
