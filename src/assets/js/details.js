071b69d3c328ec3763d0eaa69e1e06963f0f2b2a
Paul Hebert <paul@cloudfour.com>
2021-05-02 09:49:52 -0700

    Base button styles

--- src/assets/js/details.js
+++ src/assets/js/details.js
@@ -1,10 +1,28 @@
 const preview = document.querySelector('.details__graphic');
 const bonusListings = document.querySelectorAll('.bonus-listings__graphic')
 
+const refreshButton = document.querySelector(".js-refresh");
+const playButton = document.querySelector(".js-play");
+const pauseButton = document.querySelector(".js-pause");
+const downloadButton = document.querySelector(".js-download");
+
 window.customElements.whenDefined(preview.localName).then(() => {
-  document.querySelector(".js-refresh").addEventListener("click", () => preview.refreshOnce());
-  document.querySelector(".js-slideshow").addEventListener("click",  () => preview.toggle());
-  document.querySelector(".js-download").addEventListener("click", () => preview.download());
+  refreshButton.addEventListener("click", () => preview.refreshOnce());
+  downloadButton.addEventListener("click", () => preview.download());
+  playButton.addEventListener("click",  () => {
+    preview.play();
+    playButton.disabled = true;
+    refreshButton.disabled = true;
+    downloadButton.disabled = true;
+    pauseButton.disabled = false;
+  });
+  pauseButton.addEventListener("click",  () => {
+    preview.pause();
+    pauseButton.disabled = true;
+    refreshButton.disabled = false;
+    downloadButton.disabled = false;
+    playButton.disabled = false;
+  });
   
   bonusListings.forEach(listing => {
     listing.addEventListener('click', e => {
