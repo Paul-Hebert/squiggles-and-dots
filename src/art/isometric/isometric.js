707c62f45496c222d516d2e34d1bff9b709c17a6
Paul Hebert <paul@cloudfour.com>
2021-05-10 08:59:02 -0700

    Add isometric grid

---
+++ src/art/isometric/isometric.js
@@ -0,0 +1,142 @@
+import { SqPreview } from '../../assets/js/sq-preview.js'
+import {
+  random
+} from "https://cdn.skypack.dev/@georgedoescode/generative-utils@1.0.0";
+
+export class SqIsometric extends SqPreview {
+  name = "Isometric";
+  width = 200;
+  height = 100;
+
+  cubeWidth = 20;
+  cubeHeight = 10;
+  cubeDepth = 8;
+
+  addCanvas = () => {
+    this.innerHTML += /* html */`
+      <svg viewBox="0 0 200 100" class="canvas"></svg>
+    `;
+    return this.querySelector('svg');
+  }
+
+  clear = () => {
+    this.canvas.innerHTML = '';
+  }
+
+  draw = () => {
+    this.generateStyles();
+
+    let point = {x: 100, y: 20};
+
+    this.cubeRow(point, 1)
+
+    point = this.updatePoint(point);
+    this.cubeRow(point, 2)
+
+    point = this.updatePoint(point);
+    this.cubeRow(point, 3)
+
+    point = this.updatePoint(point);
+    this.cubeRow(point, 4)
+
+    point = this.updatePoint(point);
+    this.cubeRow(point, 5)
+
+    point = this.updatePoint(point, 1);
+    this.cubeRow(point, 4)
+
+    point = this.updatePoint(point, 1);
+    this.cubeRow(point, 3)
+
+    point = this.updatePoint(point, 1);
+    this.cubeRow(point, 2)
+
+    point = this.updatePoint(point, 1);
+    this.cubeRow(point, 1)
+  }
+
+  updatePoint = (point, direction = -1) => {
+    return {
+      x: point.x + this.cubeWidth/2 * direction,
+      y: point.y + this.cubeDepth/2,
+    }
+  }
+
+  cubeRow = (point, count) => {
+    let currentPoint = {...point};
+    let cubes = 0;
+
+    while(cubes < count) {
+      this.cube(currentPoint);
+      currentPoint.x += this.cubeWidth;
+      cubes++;
+    }
+  }
+
+  cube = (startPoint) => {
+    const depth = this.cubeDepth;
+    const width = this.cubeWidth;
+    const height = this.cubeHeight;
+
+    const attrs = `
+      ${this.cubeStyles[Math.round(random(0, this.cubeStyles.length - 1))]}
+      stroke="hsla(0, 0%, 50%, 20%)"
+      stroke-linejoin="bevel"
+      stroke-width="1"
+    `
+
+    const top = /* html */`
+      <path d="
+        M ${startPoint.x} ${startPoint.y}
+        l ${width/2} ${depth/-2}
+        l ${width/-2} ${depth/-2}
+        l ${width/-2} ${depth/2}
+        Z
+      "
+      ${attrs}/>
+    `;
+
+    const side1 = /* html */`
+      <path d="
+        M ${startPoint.x} ${startPoint.y}
+        l ${width/-2} ${depth/-2}
+        l 0 ${height}
+        l ${width/2} ${depth/2}
+        Z
+      "
+      ${attrs}/>
+    `;
+
+    const side2 = /* html */`
+      <path d="
+        M ${startPoint.x} ${startPoint.y}
+        l ${width/2} ${depth/-2}
+        l 0 ${height}
+        l ${width/-2} ${depth/2}
+        Z
+      "
+      ${attrs}/>
+    `;
+
+    this.canvas.innerHTML += top + side1 + side2;
+  }
+
+  randomStyle = () => {
+    const hue = random(0, 360);
+    return `
+      fill="hsl(${hue}, ${random(0, 100)}%, ${random(30, 100)}%)"
+      stroke="hsl(${hue}, ${random(0, 100)}%, ${random(20, 30)}%)"
+    `
+  }
+
+  generateStyles = () => {
+    this.cubeStyles =  [
+      this.randomStyle(),
+      this.randomStyle(),
+      this.randomStyle(),
+    ]
+  }
+}
+
+customElements.define("sq-isometric", SqIsometric);
+
---
+++ src/art/isometric/isometric.md
@@ -0,0 +1,11 @@
+---
+title: Isometric
+slug: "isometric"
+tag: "sq-isometric"
+scripts:
+  - "isometric"
+width: 200
+height: 100
+---
+
+An isometric grid of cubes. Inspired by [Into the Breach](https://subsetgames.com/itb.html), this will be expanded on in the future.
