45318741612f0c3a6044af5aadcf49a63dee5c93
Paul Hebert <paul@cloudfour.com>
2021-09-22 14:21:14 -0700

    Improve planet shadows

--- src/assets/js/bits/svg/draw-planet.js
+++ src/assets/js/bits/svg/draw-planet.js
@@ -5,8 +5,6 @@ export const drawPlanet = ({ cx, cy, size }) => {
   const id = `planet-${nanoid()}`;
   const isGas = randomBool();
   const hue = random(0, 360);
-  let secondaryHue = hue - 180;
-  if(secondaryHue < 0) secondaryHue += 360;
   const primaryColor = `hsl(${hue}, ${random(70, 100)}%, ${random(70, 90)}%)`;
 
   const scale = size;
@@ -14,8 +12,8 @@ export const drawPlanet = ({ cx, cy, size }) => {
   return /* html */`
     <defs>
       <radialGradient id="${id}-shadow">
-        <stop offset="20%" stop-color="hsla(0, 0%, 0%, 1)" />
-        <stop offset="100%" stop-color="hsla(0, 0%, 0%, 0)" />
+        <stop offset="0%" stop-color="hsla(0, 0%, 0%, 0)" />
+        <stop offset="80%" stop-color="hsla(0, 0%, 0%, 1)" />
       </radialGradient>
       <clipPath id="${id}-clip">
         <circle 
@@ -53,9 +51,9 @@ export const drawPlanet = ({ cx, cy, size }) => {
     />
     <ellipse
       fill="url(#${id}-shadow)"
-      rx="${size * 1.75}" 
-      ry="${size * 10}" 
-      cx="${cx + size}" 
+      rx="${size * 2 + 2}" 
+      ry="${size * 2 + 2}" 
+      cx="${cx - size}" 
       cy="${cy}"
       clip-path="url(#${id}-clip)"
     />
