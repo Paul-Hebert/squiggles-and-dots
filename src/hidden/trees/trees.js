777205ecb81f78af9db6d7a062620342a921ba7f
Paul Hebert <paul.hebert@paulhebertdesigns.com>
2022-04-24 13:15:09 -0700

    Hide trees
    Since it has performance issues

--- src/hidden/trees/trees.js
+++ src/hidden/trees/trees.js
@@ -19,15 +19,37 @@ export class SqTrees extends SvgCanvas {
     `;
     class TreeRenderer extends SvgRenderer {
       customCommand = (command) => {
-        if (command === "C") {
-          const h = random(0, 130);
+        if (command === "L") {
+          const h = random(40, 170);
           const s = random(50, 80);
           const l = random(20, 80);
+          const height = this.distance / random(1, 2);
+          const width = this.distance / random(2, 5);
+
           this.markup += `
-            <circle
+            <ellipse
               cx="${this.position.x}" 
-              cy="${this.position.y}" 
-              r="${this.distance / random(1, 2)}"
+              cy="${this.position.y + height / 2}" 
+              rx="${width}"
+              ry="${height}"
+              transform="rotate(${this.rotation} ${this.position.x} ${
+            this.position.y
+          })"  
+              fill="hsl(${h}, ${s}%, ${l}%)"
+            />`;
+        }
+        if (command === "B") {
+          const h = random(210, 330);
+          const s = random(70, 80);
+          const l = random(60, 80);
+          const height = this.distance * random(2, 3);
+          const width = this.distance * random(1, 1.3);
+          this.markup += `
+            <ellipse
+              cx="${this.position.x}" 
+              cy="${this.position.y + height - 1}" 
+              ry="${height}"
+              rx="${width}"
               fill="hsl(${h}, ${s}%, ${l}%)"
             />`;
         }
@@ -44,12 +66,17 @@ export class SqTrees extends SvgCanvas {
         axiom: "X",
         productions: {
           X: {
-            replacement: "F+[[XC]-X]-F[-FXC]+X",
+            // replacement: "F+L[[XB]-XL]-F[-FXBL]+XL",
+            replacement: "F[[-FXL]+XL]+F[-FXL]+XL",
             chance: "0.8",
           },
           F: {
             replacement: "FF",
             chance: "0.6",
+          },
+          L: {
+            replacement: "B",
+            chance: "0.05",
           },
         },
         iterations: random(5, 7),
@@ -57,8 +84,12 @@ export class SqTrees extends SvgCanvas {
       startRotation: random(175, 185),
       turnRotation: random(15, 30),
       distance: random(2, 6),
-      startPosition: { x: 250, y: 500 },
+      startPosition: { x: 250, y: 480 },
     });
+
+    this.canvas.innerHTML += `
+      <line x1="170" y1="480" x2="330" y2="480" stroke="black" />
+    `;
   };
 }
 
